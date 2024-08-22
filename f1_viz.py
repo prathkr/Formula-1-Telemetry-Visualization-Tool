ebea45c5ed8efdef464d077662f3b3c43975220d
Prathap Kumar <130935483+prathkr@users.noreply.github.com>
2024-08-22 19:04:04 +0530

    Update f1_viz.py

--- f1_viz.py
+++ f1_viz.py
@@ -1,166 +1,141 @@
-import tkinter as tk
-from tkinter import ttk, messagebox, simpledialog
 import fastf1 as ff1
 from fastf1 import plotting
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import matplotlib.pyplot as plt
-import pandas as pd
-import random
-import numpy as np
+from matplotlib import pyplot as plt
 from matplotlib.collections import LineCollection
-from matplotlib import cm, colors as mcolors
+from matplotlib import cm
+import matplotlib.colors as mcolors
 from matplotlib.lines import Line2D
+import numpy as np
+import pandas as pd
+import random  
 
 plotting.setup_mpl()
 ff1.Cache.enable_cache('cache')
 
-root = tk.Tk()
-root.title("F1 Telemetry Visualization Tool")
+print('ENTER RACE DATA')
+year = input('Enter year: ')
+circuit = input('Enter circuit: ')
+session = input('Sessions: FP1, FP2, FP3, Q, SQ(Sprint Qualifying), R(Race)\nEnter session: ')
+num_drivers = int(input('Enter the number of drivers to analyze: '))
 
+drivers = []
+for i in range(num_drivers):
+    driver = input('Enter 3-letter code of Driver {}: '.format(i+1))
+    drivers.append(driver)
 
-def submit():
-    try:
-        year = int(year_entry.get())
-        circuit = circuit_entry.get()
-        session = session_entry.get().upper()
-        num_drivers = int(drivers_entry.get())
-    except ValueError:
-        messagebox.showerror("Input Error", "Please enter valid data.")
-        return
+driver_colors = {
+    driver: '#{:06x}'.format(random.randint(0, 0xFFFFFF))  
+    for driver in drivers
+}
 
-    drivers = []
-    for i in range(num_drivers):
-        driver = simpledialog.askstring("Driver", f"Enter 3-letter code of Driver {i + 1}:")
-        if driver:
-            drivers.append(driver.upper())
+year = int(year)
 
-    if not drivers:
-        messagebox.showerror("Input Error", "No drivers entered.")
-        return
+race = ff1.get_session(year, '{}'.format(circuit), '{}'.format(session))
+race.load()
+laps = race.laps
 
-    driver_colors = {
-        driver: '#{:06x}'.format(random.randint(0, 0xFFFFFF))
-        for driver in drivers
-    }
+if laps is not None and not laps.empty:
+    telemetry_data = []
 
-    race = ff1.get_session(year, '{}'.format(circuit), '{}'.format(session))
-    race.load()
-    laps = race.laps
+    for driver in drivers:
+        laps_driver = race.laps.pick_driver(driver)
+        fastest_driver = laps_driver.pick_fastest().get_telemetry().add_distance()
+        fastest_driver['Driver'] = driver
+        telemetry_data.append(fastest_driver)
+        
+    telemetry = pd.concat(telemetry_data)
+    cmap = cm.colors.ListedColormap(list(driver_colors.values()))
+    num_minisectors = 25
 
-    if laps is not None and not laps.empty:
-        telemetry_data = []
+    total_distance = max(telemetry['Distance'])
 
-        for driver in drivers:
-            laps_driver = race.laps.pick_driver(driver)
-            fastest_driver = laps_driver.pick_fastest().get_telemetry().add_distance()
-            fastest_driver['Driver'] = driver
-            telemetry_data.append(fastest_driver)
+    minisector_length = total_distance / num_minisectors
 
-        telemetry = pd.concat(telemetry_data)
-        num_minisectors = 25
+    minisectors = [0]
 
-        total_distance = max(telemetry['Distance'])
-        minisector_length = total_distance / num_minisectors
+    for i in range(0, (num_minisectors - 1)):
+        minisectors.append(minisector_length * (i + 1))
 
-        telemetry['Minisector'] = telemetry['Distance'].apply(
-            lambda dist: (
-                int((dist // minisector_length) + 1)
-            )
+    telemetry['Minisector'] = telemetry['Distance'].apply(
+        lambda dist: (
+            int((dist // minisector_length) + 1)
         )
-        average_speed = telemetry.groupby(['Minisector', 'Driver'])['Speed'].mean().reset_index()
+    )
+    average_speed = telemetry.groupby(['Minisector', 'Driver'])['Speed'].mean().reset_index()
 
-        fastest_driver = average_speed.loc[average_speed.groupby(['Minisector'])['Speed'].idxmax()]
-        fastest_driver = fastest_driver[['Minisector', 'Driver']].rename(columns={'Driver': 'Fastest_driver'})
+    fastest_driver = average_speed.loc[average_speed.groupby(['Minisector'])['Speed'].idxmax()]
 
-        telemetry = telemetry.merge(fastest_driver, on=['Minisector'])
-        telemetry = telemetry.sort_values(by=['Distance'])
+    fastest_driver = fastest_driver[['Minisector', 'Driver']].rename(columns={'Driver': 'Fastest_driver'})
 
-        for i, driver in enumerate(drivers):
-            telemetry.loc[telemetry['Fastest_driver'] == driver, 'Fastest_driver_int'] = i + 1
+    telemetry = telemetry.merge(fastest_driver, on=['Minisector'])
 
-        x = np.array(telemetry['X'].values)
-        y = np.array(telemetry['Y'].values)
+    telemetry = telemetry.sort_values(by=['Distance'])
 
-        points = np.array([x, y]).T.reshape(-1, 1, 2)
-        segments = np.concatenate([points[:-1], points[1:]], axis=1)
-        fastest_driver_array = telemetry['Fastest_driver'].map(driver_colors).to_numpy()
+    for i, driver in enumerate(drivers):
+        telemetry.loc[telemetry['Fastest_driver'] == driver, 'Fastest_driver_int'] = i + 1
 
-        lc_comp = LineCollection(segments, colors=fastest_driver_array, linewidths=5)
-        c_cmap = mcolors.ListedColormap(list(driver_colors.values()))
-        plt.rcParams['figure.figsize'] = [18, 10]
-        plt.gca().add_collection(lc_comp)
-        plt.axis('equal')
-        plt.tick_params(labelleft=False, left=False, labelbottom=False, bottom=False)
+    x = np.array(telemetry['X'].values)
+    y = np.array(telemetry['Y'].values)
 
-        plt.title('{} - {} - {}'.format(circuit, year, session))
-        legend_elements = [Line2D([0], [0], color=color, label=driver) for driver, color in driver_colors.items()]
-        plt.gca().add_collection(lc_comp)
-        plt.axis('equal')
-        plt.tick_params(labelleft=False, left=False, labelbottom=False, bottom=False)
-        plt.legend(handles=legend_elements, loc='upper left')
-        drivers = telemetry['Driver'].unique()
+    points = np.array([x, y]).T.reshape(-1, 1, 2)
+    segments = np.concatenate([points[:-1], points[1:]], axis=1)
+    fastest_driver_array = telemetry['Fastest_driver'].map(driver_colors).to_numpy()
 
-        plt.figure(figsize=(12, 8))
-        for driver in drivers:
-            driver_data = telemetry[telemetry['Driver'] == driver]
-            plt.plot(driver_data['Distance'], driver_data['Speed'], label=driver, color=driver_colors[driver])
+    lc_comp = LineCollection(segments, colors=fastest_driver_array, linewidths=5)
+    c_cmap = mcolors.ListedColormap(list(driver_colors.values()))   
+    plt.rcParams['figure.figsize'] = [18, 10]
+    plt.gca().add_collection(lc_comp)
+    plt.axis('equal')
+    plt.tick_params(labelleft=False, left=False, labelbottom=False, bottom=False)
 
-        legend_elements = [plt.Line2D([0], [0], marker='o', color='w', label=driver,
-                                      markerfacecolor=driver_colors[driver], markersize=10)
-                           for driver in drivers]
-        plt.legend(handles=legend_elements, loc='upper left')
-        plt.title('{} - {} - {}'.format(circuit, year, session))
-        plt.xlabel('Distance')
-        plt.ylabel('Speed')
-        plt.grid(True)
-        plt.savefig('driver_speed_graph.png', dpi=300)
-        plt.show()
+    
+    plt.title('{} - {} - {}'.format(circuit,year,session))
+    legend_elements = [Line2D([0], [0], color=color, label=driver) for driver, color in driver_colors.items()]
+    plt.gca().add_collection(lc_comp)
+    plt.axis('equal')
+    plt.tick_params(labelleft=False, left=False, labelbottom=False, bottom=False)
+    plt.legend(handles=legend_elements, loc='upper left')  
+    drivers = telemetry['Driver'].unique()
+    
+    plt.figure(figsize=(12, 8))
+    for driver in drivers:
+        driver_data = telemetry[telemetry['Driver'] == driver]
+        plt.plot(driver_data['Distance'], driver_data['Speed'], label=driver, color=driver_colors[driver])
 
-        plt.figure(figsize=(12, 8))
-        for driver in drivers:
-            driver_data = telemetry[telemetry['Driver'] == driver]
-            plt.plot(driver_data['Distance'], driver_data['Throttle'], label=driver, color=driver_colors[driver])
-        plt.title('{} - {} - {}'.format(circuit, year, session))
-        plt.xlabel('Distance')
-        plt.ylabel('Throttle')
-        plt.grid(True)
-        plt.legend(loc='upper left')
-        plt.savefig('driver_throttle_graph.png', dpi=300)
-        plt.show()
+    legend_elements = [plt.Line2D([0], [0], marker='o', color='w', label=driver, 
+                               markerfacecolor=driver_colors[driver], markersize=10) 
+                   for driver in drivers]
+    plt.legend(handles=legend_elements, loc='upper left')
+    plt.title('{} - {} - {}'.format(circuit,year,session))
+    plt.xlabel('Distance')
+    plt.ylabel('Speed')
+    plt.grid(True)
+    plt.savefig('driver_speed_graph.png', dpi=300)
+    plt.show()
+    
+    plt.figure(figsize=(12, 8))
+    for driver in drivers:
+        driver_data = telemetry[telemetry['Driver'] == driver]
+        plt.plot(driver_data['Distance'], driver_data['Throttle'], label=driver, color=driver_colors[driver])
+    plt.title('{} - {} - {}'.format(circuit,year,session))
+    plt.xlabel('Distance')
+    plt.ylabel('Throttle')
+    plt.grid(True)
+    plt.legend(loc='upper left')
+    plt.savefig('driver_throttle_graph.png', dpi=300)
+    plt.show()
 
-        plt.figure(figsize=(12, 8))
-        for driver in drivers:
-            driver_data = telemetry[telemetry['Driver'] == driver]
-            plt.plot(driver_data['Distance'], driver_data['Brake'], label=driver, color=driver_colors[driver])
-        plt.title('{} - {} - {}'.format(circuit, year, session))
-        plt.xlabel('Distance')
-        plt.ylabel('Brake')
-        plt.grid(True)
-        plt.legend(loc='upper left')
-        plt.savefig('driver_brake_graph.png', dpi=300)
-        plt.show()
+    plt.figure(figsize=(12, 8))
+    for driver in drivers:
+        driver_data = telemetry[telemetry['Driver'] == driver]
+        plt.plot(driver_data['Distance'], driver_data['Brake'], label=driver, color=driver_colors[driver])
+    plt.title('{} - {} - {}'.format(circuit,year,session))
+    plt.xlabel('Distance')
+    plt.ylabel('Brake')
+    plt.grid(True)
+    plt.legend(loc='upper left')
+    plt.savefig('driver_brake_graph.png', dpi=300)
+    plt.show()
 
-    else:
-        print("Failed to load session data.")
-
-
-ttk.Label(root, text="Enter year:").grid(row=0, column=0, padx=10, pady=5, sticky='e')
-year_entry = ttk.Entry(root)
-year_entry.grid(row=0, column=1, padx=10, pady=5)
-
-ttk.Label(root, text="Enter circuit:").grid(row=1, column=0, padx=10, pady=5, sticky='e')
-circuit_entry = ttk.Entry(root)
-circuit_entry.grid(row=1, column=1, padx=10, pady=5)
-
-ttk.Label(root, text="Enter session (FP1, FP2, FP3, Q, SQ, R):").grid(row=2, column=0, padx=10, pady=5, sticky='e')
-session_entry = ttk.Entry(root)
-session_entry.grid(row=2, column=1, padx=10, pady=5)
-
-ttk.Label(root, text="Enter the number of drivers to analyze:").grid(row=3, column=0, padx=10, pady=5, sticky='e')
-drivers_entry = ttk.Entry(root)
-drivers_entry.grid(row=3, column=1, padx=10, pady=5)
-
-submit_button = ttk.Button(root, text="Submit", command=submit)
-submit_button.grid(row=4, column=0, columnspan=2, pady=10)
-
-root.mainloop()
+else:
+    print("Failed to load session data.")
